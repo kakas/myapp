1f88828b24ff90b213660d93b4af342f62fda28f
kakas <shrimptrain@gmail.com>
2017-12-11 14:45:31 +0800

    gem faker and add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  Post.create(title: Faker::Name.name, content: Faker::Name.name)
+end
